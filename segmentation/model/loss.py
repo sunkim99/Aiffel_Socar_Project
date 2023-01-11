10e0ea3c2e9c9b19accab42ba87531a0b050c1d7
imLim <imlim0813@hanyang.ac.kr>
2023-01-12 01:40:41 +0900

    updated version

--- segmentation/model/loss.py
+++ segmentation/model/loss.py
@@ -91,3 +91,35 @@ class FocalTverskyLoss(nn.Module):
         FocalTversky = (1 - Tversky)**gamma
                        
         return FocalTversky
+
+
+class IoULoss(nn.Module):
+    """
+    Description
+     : IOU Loss
+
+    Parameters
+     : inputs : predictions.
+     : targets : targets.
+    """
+    def __init__(self, weight=None, size_average=True):
+        super(IoULoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = torch.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #intersection is equivalent to True Positive count
+        #union is the mutually inclusive area of all labels & predictions 
+        intersection = (inputs * targets).sum()
+        total = (inputs + targets).sum()
+        union = total - intersection 
+        
+        IoU = (intersection + 1e-6)/(union + 1e-6)
+                
+        return 1 - IoU
